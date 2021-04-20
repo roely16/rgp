d33713b0f3b62f9c5eb3b70c474a6857da2fff33
Roely Chur <gerson.roely@gmail.com>
2021-04-19 18:15:43 -0600

    First

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<v-app>
+			<v-main>
+				<v-container fluid>
+					<v-row>
+						<v-col cols="6">
+							<v-text-field v-model="search" prepend-inner-icon="mdi-magnify" label="Buscar..." outlined></v-text-field>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-col>
+							<v-data-table :search="search" :headers="headers" :items="items" :loading="loading">
+								<template v-slot:[`item.operaciones`]="{ item }">
+								
+								<v-row v-for="(el, key) in item.operaciones" :key="key">
+									<v-col cols="12" v-for="(i, key) in el" :key="key">
+										<strong>{{ key }}:</strong>
+										<br>
+										{{i }}
+									</v-col>
+								</v-row>
+
+								</template>
+
+								<template  v-slot:[`item.otorgantes`]="{ item }">
+								
+								<v-row v-for="(el, key) in item.otorgantes" :key="key">
+									<v-col cols="12" v-for="(i, key) in el" :key="key">
+										<strong>{{ key }}:</strong>
+										<br>
+										{{i }}
+									</v-col>
+								</v-row>
+
+								</template>
+
+								<template  v-slot:[`item.bienes`]="{ item }">
+								
+								<v-row v-for="(el, key) in item.bienes" :key="key">
+									<v-col cols="12" v-for="(i, key) in el" :key="key">
+										<strong>{{ key }}:</strong>
+										<br>
+										{{i }}
+									</v-col>
+								</v-row>
+
+								</template>
+							</v-data-table>
+						</v-col>
+					</v-row>
+				</v-container>
+			
+			</v-main>
+		</v-app>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	data(){
+		return{
+		items: [],
+		loading: false,
+		headers: [],
+		search: null
+		}
+	},
+	methods: {
+		obtener_datos(){
+
+			this.loading = true 
+
+			fetch("data.json")
+			.then(res => res.json())
+			.then(data => {
+
+				this.items = data
+				this.loading = false
+
+				const keys = Object.keys(data[0]);
+
+				keys.forEach(key => {
+					
+					let element = {
+						text: key,
+						value: key
+					}
+
+					this.headers.push(element)
+
+				});
+
+			})
+
+		
+		}
+	},
+	created(){
+
+		this.obtener_datos()
+
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
